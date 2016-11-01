b67dc10c235cf822d00f04457179845c91549603
Jakub <jkb@eaaa.dk>
2016-11-01 11:34:56 +0100

    Main project start

---
+++ Prototype/SmartWatch/DataWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static Prototype1.MyClasses;
+
+namespace Prototype1
+{
+    /// <summary>
+    /// Interaction logic for DataWindow.xaml
+    /// </summary>
+    public partial class DataWindow : Window
+    {
+        public DataWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            groupBox.DataContext = comboBox.SelectedItem;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            comboBox.ItemsSource = Service.Instance.Watches;
+        }
+    }
+}
